dea1f06469d047fc4df7c700b69256241c932b00
Maxdeoro <maxdeoro@gmail.com>
2024-12-03 23:57:44 +0300

    add tabs

--- js/script.js
+++ js/script.js
@@ -1,7 +1,8 @@
-$('#date').datepicker();
+$('#date').datepicker({});
 $('#accordion').accordion();
 $('#button').button();
 $('#button-icon').button({
     icon: 'ui-icon-gear',
     showLabel: false,
 });
+$('#tabs').tabs();
